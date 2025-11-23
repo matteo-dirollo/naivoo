3f08bee24103e94e62eceb4d9d08773bcc1a49cb
matteo-dirollo <matteo.dirollo@accademiabellearti.fr.it>
2025-11-23 23:03:15 +0100

    Redirection to home when signed in

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,7 +1,12 @@
-import {Stack} from "expo-router";
+import {Stack, Redirect} from "expo-router";
+import { useAuth } from "@clerk/clerk-expo";
 
 const Layout = () => {
+    const { isSignedIn } = useAuth();
 
+    if (isSignedIn) {
+        return <Redirect href="/(root)/(tabs)/home" />;
+    }
     return (
         <Stack>
             <Stack.Screen name="welcome" options={{headerShown: false}}/>
