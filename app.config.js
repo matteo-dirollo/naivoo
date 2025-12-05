da9a230549bf308bcde720874c5969393380bc9e
Matteo Di Rollo <matteo.dirollo@icloud.com>
2025-12-05 21:53:21 +0100

    New images

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     slug: "naivoo",
     version: "1.0.0",
     orientation: "portrait",
-    icon: "./assets/icons/adaptive.png",
+    icon: "./assets/images/naivoo-logo_v1.png.png",
     scheme: "naivoo",
     userInterfaceStyle: "automatic",
     newArchEnabled: true,
@@ -29,7 +29,7 @@ export default {
         },
       },
       adaptiveIcon: {
-        foregroundImage: "./assets/images/adaptive-icon.png",
+        foregroundImage: "./assets/images/naivoo-logo_v1.png.png",
         backgroundColor: "#ffffff",
       },
       permissions: [
