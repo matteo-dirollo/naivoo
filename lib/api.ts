f733556f7b7e2990c27451470802ec2c9e3458b6
matteo-dirollo <matteo.dirollo@accademiabellearti.fr.it>
2025-12-13 00:23:11 +0100

    API route fix

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL:
-    process.env.EXPO_PUBLIC_API_BASE_URL ||
-    "https://9lcex5k-matteo92-8081.exp.direct", // Fallback to a default value
+    process.env.EXPO_PUBLIC_API_BASE_URL, // Fallback to a default value
   timeout: 10000, // 10 seconds timeout
 });
